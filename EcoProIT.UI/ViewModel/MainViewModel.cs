3b2fafa93e84c487d34633879f331d69df26bc80
Jon Larborn <jon.larborn@varderingsdata.se>
2025-12-03 15:43:22 +0100

    Add TODO comments about ClickOnce and .NET 5+ limitations
    Added TODO comments in DESMachine.cs and MainViewModel.cs noting that, although ClickOnce is supported on .NET 5+, applications cannot access the System.Deployment.Application namespace. References to related GitHub issues are included for further details. No functional code changes were made.

--- EcoProIT.UI/ViewModel/MainViewModel.cs
+++ EcoProIT.UI/ViewModel/MainViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+// TODO Although ClickOnce is supported on .NET 5+, apps do not have access to the System.Deployment.Application namespace. For more details see https://github.com/dotnet/deployment-tools/issues/27 and https://github.com/dotnet/deployment-tools/issues/53.
 using System.Deployment.Application;
 using System.IO;
 using System.Linq;
